0aa530cf354b2901cac8e6902e435e3a47d86ce7
Sercan Turkmen <sercanturkmen@outlook.com>
2018-07-23 13:33:02 +0300

    reformat code

--- library/src/test/java/com/aware/android/sensor/timezone/ExampleUnitTest.kt
+++ library/src/test/java/com/aware/android/sensor/timezone/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.aware.android.sensor.timezone
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
